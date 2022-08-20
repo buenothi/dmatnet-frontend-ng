94615388994650a2608a72115c6837ae3ced7da3
thiagobueno <bueno_thiago@outlook.com>
2022-08-20 15:51:34 -0300

    Ajuestes CSS Menu e Forms Exibir

--- src/app/main/menu-modulos/menu-modulos.component.ts
+++ src/app/main/menu-modulos/menu-modulos.component.ts
@@ -9,15 +9,36 @@ import { MainComponent } from '../main.component';
 export class MenuModulosComponent implements OnInit {
 
   constructor(
-    private mainComponent:MainComponent
+    private mainComponent: MainComponent
   ) { }
 
   cssMenuLateral = 'lateral'
   menuBotao = 'logoMenu'
   textoMenuBotao = 'Menu'
-  cssMenuBotao = 'botao menuBotao'
+
+  cssBtnMenu = 'botao menuBotao'
+  cssBtnHome = 'botao menuBotao'
+  cssBtnEmpresa = 'botao menuBotao'
+  cssBtnUsuario = 'botao menuBotao'
+  cssBtnRegAmbiental = 'botao menuBotao'
+  cssBtnSaudeTrabalho = 'botao menuBotao'
+  cssBtnDocLegais = 'botao menuBotao'
+  cssBtnAcidentes = 'botao menuBotao'
+  cssBtnEqProtecao = 'botao menuBotao'
+  cssBtnFerramentas = 'botao menuBotao'
+  cssBtnTreinamento = 'botao menuBotao'
 
   ngOnInit(): void {
+    this.cssBtnHome = 'botao menuBotao'
+    this.cssBtnEmpresa = 'botao menuBotao'
+    this.cssBtnUsuario = 'botao menuBotao'
+    this.cssBtnRegAmbiental = 'botao menuBotao'
+    this.cssBtnSaudeTrabalho = 'botao menuBotao'
+    this.cssBtnDocLegais = 'botao menuBotao'
+    this.cssBtnAcidentes = 'botao menuBotao'
+    this.cssBtnEqProtecao = 'botao menuBotao'
+    this.cssBtnFerramentas = 'botao menuBotao'
+    this.cssBtnTreinamento = 'botao menuBotao'
   }
 
   openMenu(): void {
@@ -25,14 +46,59 @@ export class MenuModulosComponent implements OnInit {
       this.cssMenuLateral = 'lateralFixo'
       this.menuBotao = 'logoMenuFixo'
       this.textoMenuBotao = 'Desafixar Menu'
-      this.cssMenuBotao = 'botao menuBotaoSelecionado'
+      this.cssBtnMenu = 'botao menuBotaoSelecionado'
       this.mainComponent.changeConteudo()
     } else {
       this.cssMenuLateral = 'lateral'
       this.menuBotao = 'logoMenu'
       this.textoMenuBotao = 'Menu'
-      this.cssMenuBotao = 'botao menuBotao'
+      this.cssBtnMenu = 'botao menuBotao'
       this.mainComponent.changeConteudo()
+    }
+  }
+
+  onClickBtn(moduloNome:string):void {
+    switch (moduloNome) {
+      case 'home':
+        this.ngOnInit();
+        this.cssBtnHome = 'botao menuBotaoSelecionado';
+        break;
+      case 'empresa':
+        this.ngOnInit();
+        this.cssBtnEmpresa = 'botao menuBotaoSelecionado';
+        break;
+      case 'usuario':
+        this.ngOnInit();
+        this.cssBtnUsuario = 'botao menuBotaoSelecionado';
+        break;
+      case 'ambiental':
+        this.ngOnInit();
+        this.cssBtnRegAmbiental = 'botao menuBotaoSelecionado';
+        break;
+      case 'saude':
+        this.ngOnInit();
+        this.cssBtnSaudeTrabalho = 'botao menuBotaoSelecionado';
+        break;
+      case 'documentos':
+        this.ngOnInit();
+        this.cssBtnDocLegais = 'botao menuBotaoSelecionado';
+        break;
+      case 'acidente':
+        this.ngOnInit();
+        this.cssBtnAcidentes = 'botao menuBotaoSelecionado';
+        break;
+      case 'eqpto':
+        this.ngOnInit();
+        this.cssBtnEqProtecao = 'botao menuBotaoSelecionado';
+        break;
+      case 'ferramentas':
+        this.ngOnInit();
+        this.cssBtnFerramentas = 'botao menuBotaoSelecionado';
+        break;
+      case 'treinamento':
+        this.ngOnInit();
+        this.cssBtnTreinamento = 'botao menuBotaoSelecionado';
+        break;
     }
   }
 
